b98457052e79554d10804f8ee84b21e86e709292
papayaw007 <papa.adu-asare@ashesi.edu.gh>
2025-04-08 02:52:04 +0000

    api update

--- main.js
+++ main.js
@@ -113,7 +113,7 @@ async function generatePasswordWithPrompt(promptString) {
     : 'https://ai-password-generator-backend.onrender.com';
 
     try {
-      const response =  await fetch(API_URL, {
+      const response =  await fetch(window.appConfig.apiUrl, {
         method: "POST",
         headers: {
           "Content-Type": "application/json"
